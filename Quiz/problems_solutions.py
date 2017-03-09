db6b485cd22288c1129b4482bb1238e02d85a724
Claudio Castello <claudio.chemist@gmail.com>
2017-03-09 12:48:53 -0300

    Update and rename problem3.py to problems_solutions.py

---
+++ Quiz/problems_solutions.py
@@ -0,0 +1,59 @@
+#
+# Problem 3
+#
+def song_playlist(songs, max_size):
+    '''
+    songs: list of tuples, ('song_name', song_len, song_size)
+    max_size: float, maximum size of total songs that you can fit
+    
+    Start with the song first in the 'songs' list, then pick the next
+    song to be the one with the lowest file size not already picked, repeat.
+
+    return: a list of a subset of songs fitting in max_size in the order
+            in which they were chosen.
+    '''
+    songList = []
+    if songs == [] or songs[0][2] > max_size:
+        return []
+    else:
+        songList.append(songs[0][0])
+        tempSongList = songs[1:]
+        orderedRemainingSongs = sorted(tempSongList, key=lambda tup: tup[2])
+        currentSize = max_size - songs[0][2]
+        for i in range(len(orderedRemainingSongs)):
+            if currentSize - orderedRemainingSongs[i][2] < 0:
+                break
+            else:
+                songList.append(orderedRemainingSongs[i][0])
+                currentSize -= orderedRemainingSongs[i][2]
+        return songList
+
+#
+# Problem 4
+#
+def greedySum(L, s):
+    """ input: s, positive integer, what the sum should add up to
+               L, list of unique positive integers sorted in descending order
+        Use the greedy approach where you find the largest multiplier for 
+        the largest value in L then for the second largest, and so on to 
+        solve the equation s = L[0]*m_0 + L[1]*m_1 + ... + L[n-1]*m_(n-1)
+        return: the sum of the multipliers or "no solution" if greedy approach does 
+                not yield a set of multipliers such that the equation sums to 's'
+    """
+    tempMultipliers = []
+    tempSum = s
+    for i in range(len(L)):
+        tempMultipliers.append(0)
+        listInput = L[i]
+        if tempSum - listInput >= 0:
+            diff = tempSum - listInput
+            multiplier = 1
+            while diff >= 0 and diff >= listInput:
+                multiplier += 1
+                diff -= listInput
+            tempSum = diff
+            tempMultipliers[i] = multiplier
+    if tempSum == 0:
+        return sum(tempMultipliers)
+    else:
+        return 'no solution'
